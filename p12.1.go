831effa00c505ab7bc47644511f3ac1152803af8
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-03-13 23:02:45 +0000

    added description for p12.1

--- p12.1.go
+++ p12.1.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+/**
+ *  Problem 12.1: Dictionaries.
+ *  Input Description: A set of n records, each identified by one or more key fields.
+ *  Problem Description: Build and maintain a data structure to efficiently locate, insert, and delete the record associated with any query key q.
+ */
+
 func Problem12_1() {
     fmt.Println(" -- Problem 12.1 -- ")
 }
