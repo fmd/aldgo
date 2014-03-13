97e87afbfd29fd069e61b5509514f64c8f4f60e3
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-03-13 16:49:27 +0000

    updated with proper appends and fixed formatting for the output

--- main.go
+++ main.go
@@ -45,18 +45,19 @@ func problem1_2() {
 
     // Set up an example problem set and test the method.
     problemSet := []Interval{}
-    problemSet[0] = Interval{"The President's Algorist", 0, 5}
-    problemSet[1] = Interval{"Discrete Mathematics", 1, 3}
-    problemSet[2] = Interval{"Tarjan of the Jungle", 2, 7}
-    problemSet[3] = Interval{"Halting State", 4, 8}
-    problemSet[4] = Interval{"Steiner's Tree", 6, 10}
-    problemSet[5] = Interval{"The Four Volume Problem", 9, 15}
-    problemSet[6] = Interval{"Programming Challenges", 11, 13}
-    problemSet[7] = Interval{"Process Terminated", 12, 16}
-    problemSet[8] = Interval{"Calculated Bets", 14, 17}
+    problemSet = append(problemSet, Interval{"The President's Algorist", 0, 5})
+    problemSet = append(problemSet, Interval{"Discrete Mathematics", 1, 3})
+    problemSet = append(problemSet, Interval{"Tarjan of the Jungle", 2, 7})
+    problemSet = append(problemSet, Interval{"Halting State", 4, 8})
+    problemSet = append(problemSet, Interval{"Steiner's Tree", 6, 10})
+    problemSet = append(problemSet, Interval{"The Four Volume Problem", 9, 15})
+    problemSet = append(problemSet, Interval{"Programming Challenges", 11, 13})
+    problemSet = append(problemSet, Interval{"Process Terminated", 12, 16})
+    problemSet = append(problemSet, Interval{"Calculated Bets", 14, 17})
 
-    subset = rightJobs(problemSet)
-    for s := range subset {
-        fmt.Println(s.Label, ": ", strconv.Itoa(s.Start), strconv.Itoa(s.End))
+    subset := rightJobs(problemSet)
+    for idx := range subset {
+        s := subset[idx]
+        fmt.Println(s.Label, ": ", strconv.Itoa(s.Start), " -> ", strconv.Itoa(s.End))
     }
 }
