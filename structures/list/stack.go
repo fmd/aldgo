8695663d0fdfc0dd4c398cbbfed374f593b1c6de
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-03-17 10:08:31 +0000

    updated stack

--- structures/list/stack.go
+++ structures/list/stack.go
@@ -6,4 +6,21 @@ type Stack interface {
 }
 
 func (s *Singly) Push(item interface{}) {
+    s.Insert(item)
+}
+
+func (s *Singly) Pop() interface{} {
+    item := s.First.Item
+    s.Delete(item)
+    return item
+}
+
+func (d *Doubly) Push(item interface{}) {
+    d.InsertBeginning(item)
+}
+
+func (d *Doubly) Pop() interface{} {
+    item := d.First.Item
+    d.Delete(item)
+    return item
 }
