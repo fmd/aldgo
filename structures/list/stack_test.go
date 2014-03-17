211df607385e41019eedad40419a97bdbc131170
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-03-17 10:49:33 +0000

    updated stack test - a lot easier to read now

--- structures/list/stack_test.go
+++ structures/list/stack_test.go
@@ -2,57 +2,36 @@ package list
 
 import "testing"
 
-func createEmptySinglyStack() *Singly {
-    return &Singly{}
-}
-
-func createEmptyDoublyStack() *Doubly {
-   return &Doubly{}
-}
-
-func TestSinglyStackPush(t *testing.T) {
-    s := createEmptySingly()
-    s.Push(5)
-    s.Push(6)
-    s.Push(7)
-
-    if s.First.Item != 7 {
-        t.Error("Pushing new items onto a stack has failed.")
-    }
-}
-
-func TestSinglyStackPop(t *testing.T) {
-    s := createEmptySingly()
+func GenericStackPushPop(s Stack) string {
     s.Push(5)
     s.Push(6)
     s.Push(7)
 
     item := s.Pop()
     if item != 7 {
-        t.Error("Popping items from a stack has failed.")
+        return "Popping items from the stack has failed."
     }
 
-    if s.First.Item != 6 {
-        t.Error("Popped item was not properly removed from the stack.")
+    item = s.Pop()
+    if item != 6 {
+        return "Popped item was not properly removed from the stack"
+    }
+
+    return ""
+}
+
+func TestSinglyStackPushPop(t *testing.T) {
+    s := &Singly{}
+    e := GenericStackPushPop(s)
+    if len(e) > 0 {
+        t.Error(e)
     }
 }
 
-func TestDoublyStackPush(t *testing.T) {
-    d := createEmptyDoublyStack()
-    d.Push(5)
-    d.Push(6)
-    d.Push(7)
-
-    if d.First.Item != 7 {
-        t.Error("Pushing new items onto a stack using a Doubly-Linked List has failed.")
+func TestDoublyStackPushPop(t *testing.T) {
+    d := &Doubly{}
+    e := GenericStackPushPop(d)
+    if len(e) > 0 {
+        t.Error(e)
     }
-}
-
-func TestDoublyStackPop(t *testing.T) {
-    d := createEmptyDoublyStack()
-    d.Push(5)
-    d.Push(6)
-    d.Push(7)
-
-    item := d.Pop
 }
