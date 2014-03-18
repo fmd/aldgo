0faf9fbedc82d3d63143373535874d4db695a3b4
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-03-18 11:10:27 +0000

    updated binst testing

--- structures/tree/binst_test.go
+++ structures/tree/binst_test.go
@@ -11,17 +11,17 @@ func (i BstInt) Value() interface{} {
     return i.value
 }
 
-func (i BstInt) LessThan(b binst.TreeItem) bool {
+func (i BstInt) Compare(b binst.TreeItem) bool {
     return i.value < b.Value().(int)
 }
 
 func TestBinaryTree(t *testing.T) {
     b := &binst.Tree{BstInt{5},nil,nil}
-    binst.Insert(BstInt{4}, &b)
-    binst.Insert(BstInt{6}, &b)
-    binst.Insert(BstInt{3}, &b)
-    binst.Insert(BstInt{5}, &b)
-    binst.Insert(BstInt{19}, &b)
+    binst.Insert(BstInt{4}, &b, nil)
+    binst.Insert(BstInt{6}, &b, nil)
+    binst.Insert(BstInt{3}, &b, nil)
+    binst.Insert(BstInt{5}, &b, nil)
+    binst.Insert(BstInt{19}, &b, nil)
 
     if b.Minimum().Item.Value() != 3 {
         t.Error("Minimum function did not work on Binary Search Tree")
