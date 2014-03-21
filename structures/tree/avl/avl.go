155599a98e19b9eaa4d8fe756a942d10675ac0bb
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-03-21 17:06:00 +0000

    updated avl

--- structures/tree/avl/avl.go
+++ structures/tree/avl/avl.go
@@ -1,16 +1,20 @@
 package avl
 
-import "github.com/fmd/aldgo/structures/tree/binst"
+import "github.com/fmd/aldgo/structures/tree/bst"
 
-type TreeItem binst.TreeItem
-type Tree bst.Tree
+type Tree struct {
+    Item bst.TreeItem
+    Left *Tree
+    Right *Tree
+    Parent *Tree
+}
 
-func (t Tree) BalanceFactor() int {
+func (t *Tree) BalanceFactor() int {
     return t.Left().Height() - t.Right().Height()
 }
 
-func (t Tree) RightRotate() {
-    var l *binst.Tree
+func (t *Tree) RightRotate() {
+    var l *Tree
     if t.Left() != nil {
         l = t.Left()
     }
@@ -24,8 +28,8 @@ func (t Tree) RightRotate() {
     t.Parent() = l
 }
 
-func (t Tree) LeftRotate() {
-    var r Tree
+func (t *Tree) LeftRotate() {
+    var r *Tree
     if t.Right() != nil {
         r = t.Right()
     }
@@ -39,29 +43,29 @@ func (t Tree) LeftRotate() {
     t.Parent() = l
 }
 
-func (t Tree) Balance() {
+func (t *Tree) Balance() {
     if t.BalanceFactor() == 2 {
-        var l Tree
+        var l *Tree
         if t.Left() != nil {
             l = t.Left()
             if l.BalanceFactor() == -1 {
-                l.Left()Rotate()
+                l.Left().Rotate()
             }
         }
-        t.Right()Rotate()
+        t.Right().Rotate()
     } else {
-        var r Tree
+        var r *Tree
         if t.Right() != nil {
             r = t.Right()
             if r.BalanceFactor() == -1 {
-                bstr.Right()Rotate()
+                bstr.Right().Rotate()
             }
         }
-        t.Left()Rotate()
+        t.Left().Rotate()
     }
 }
 
-func Insert(x TreeItem, t *Tree, parent Tree) {
+func Insert(x bst.TreeItem, t **Tree, parent *Tree) {
     binst.Insert(x, t, parent)
     (*t).Balance()
 }
